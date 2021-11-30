4881a1db97b540056e0e3c7daeaa6efc2fb2d6f4
thisconnect <p7tryk@gmail.com>
2021-11-30 12:37:16 +0100

    Lib86: New demo using int 23h service1

--- emulator/main.cpp
+++ emulator/main.cpp
@@ -15,12 +15,12 @@ int main(int argc, char **argv)
   test(&cpu);
   auto ip = cpu.ip();
   printf("readAt 0x%2x = %d\nreadAt 0x%2x = %d\n", ip, cpu.readAt<uint8_t>(ip), ip+1, cpu.readAt<uint8_t>(ip+1));
-  cpu.dumpmemory(0x100,0x80);
-  cpu.dump();
-  cpu.next();
-  cpu.dump();
-  cpu.next();
-  cpu.dump();
+  //cpu.dumpmemory(0x100,0x80);
+  while(true)
+    {
+      cpu.dump();
+      cpu.next();
+    }
 
   return 0;
 }
@@ -32,17 +32,26 @@ void test(Lib86::CPU * cpu)
   cpu->writeAt<uint8_t>(ip,0x80);
   cpu->writeAt<uint8_t>(ip+1,0xc2);
   cpu->writeAt<uint8_t>(ip+2,0x16);
-  //INT 23h (DOS exit)
-  cpu->writeAt<uint8_t>(ip+3,0xCD);
-  cpu->writeAt<uint8_t>(ip+4,0x23);
   //ADD DL, CH
   cpu->writeAt<uint8_t>(ip+5,0x02);
   cpu->writeAt<uint8_t>(ip+6,0xD5);
-  //ADD AL, bh
+  //ADD AL, BH
   cpu->writeAt<uint8_t>(ip+7,0x02);
   cpu->writeAt<uint8_t>(ip+8,0xc7);
 
-  cpu->set_ip(ip+5);
+  cpu->set_ip(ip+7);
+
+
+  //POP AX
+  cpu->writeAt<uint8_t>(ip+9,0x8f);
+  cpu->writeAt<uint8_t>(ip+10,0xc0);
+  //ADD AH, CH
+  cpu->writeAt<uint8_t>(ip+11,0x02);
+  cpu->writeAt<uint8_t>(ip+12,0xe5);
+  //int 21h
+  cpu->writeAt<uint8_t>(ip+13, 0xCD);
+  cpu->writeAt<uint8_t>(ip+14, 0x21);
+
 }
 
 
