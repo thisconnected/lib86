135174ab2a3b2aac6d0e9326336b1959ce2a5a96
thisconnect <p7tryk@gmail.com>
2021-11-30 12:34:23 +0100

    Lib86: add support for 16bit instructions

--- lib86/instruction.cpp
+++ lib86/instruction.cpp
@@ -66,16 +66,20 @@ namespace Lib86 {
       {
       case RM_TABLE_1:
 	printf ("\tMOD = RM_TABLE_1\n");
+	size+=1;
 	break;
       case RM_TABLE_2_BYTE:
 	printf ("\tMOD = RM_TABLE_2_BYTE\n");
+	size+=3;
 	break;
       case RM_TABLE_2_WORD:
 	printf ("\tMOD = RM_TABLE_2_WORD\n");
+	size+=3;
 	break;
       case REG:
 	printf ("\tMOD = REG\n");
 	m_second = (uint16_t*) getRegister(rm());
+	size++;
 	break;
       default:
 	std::cerr << "\tUnrecognized Mod field of " << std::bitset<8>(m_mod) << std::endl;
@@ -90,49 +94,91 @@ namespace Lib86 {
     //HACK: this just displays instead of getting the correct pointer
     std::cout << "\tInstruction::getRegister(" << std::bitset<8>(REG) << ")" << std::endl << "\t";
 
+
+    //this should be a jumptable to be faster
     if(word())
-      assert("word size" && false);
-
-    switch(REG)
       {
-      case AL:
-	std::cout << "\tREG "<< std::bitset<3>(REG) << " = AL" << std::endl;
-	retval = fakeCPU->al();
-	break;
-      case CL:
-	std::cout << "\tREG "<< std::bitset<3>(REG) << " = CL" << std::endl;
-	retval = fakeCPU->cl();
-	break;
-      case DL:
-	std::cout << "\tREG "<< std::bitset<3>(REG) << " = DL" << std::endl;
-	retval = fakeCPU->dl();
-	break;
-      case BL:
-	std::cout << "\tREG "<< std::bitset<3>(REG) << " = BL" << std::endl;
-	retval = fakeCPU->bl();
-	break;
+	switch(REG)
+	  {
+	  case AL:
+	    std::cout << "\tREG "<< std::bitset<3>(REG) << " = AX" << std::endl;
+	    retval = fakeCPU->ax();
+	    break;
+	  case CL:
+	    std::cout << "\tREG "<< std::bitset<3>(REG) << " = CX" << std::endl;
+	    retval = fakeCPU->cx();
+	    break;
+	  case DL:
+	    std::cout << "\tREG "<< std::bitset<3>(REG) << " = DX" << std::endl;
+	    retval = fakeCPU->dx();
+	    break;
+	  case BL:
+	    std::cout << "\tREG "<< std::bitset<3>(REG) << " = BX" << std::endl;
+	    retval = fakeCPU->bx();
+	    break;
+	    
+	  case AH:
+	    std::cout << "\tREG "<< std::bitset<3>(REG) << " = SP" << std::endl;
+	    retval = fakeCPU->sp();
+	    break;
+	  case CH:
+	    std::cout << "\tREG "<< std::bitset<3>(REG) << " = BP" << std::endl;
+	    retval = fakeCPU->bp();
+	    break;
+	  case DH:
+	    std::cout << "\tREG "<< std::bitset<3>(REG) << " = SI" << std::endl;
+	    retval = fakeCPU->si();
+	    break;
+	  case BH:
+	    std::cout << "\tREG "<< std::bitset<3>(REG) << " = DI" << std::endl;
+	    retval = fakeCPU->di();
+	    break;
+	    
+	  default:
+	    std::cerr << "Unrecognized REG field of " << std::bitset<3>(REG) << std::endl;
+	  }
+      }
+    else
+      {
+        switch(REG)
+	  {
+	  case AL:
+	    std::cout << "\tREG "<< std::bitset<3>(REG) << " = AL" << std::endl;
+	    retval = fakeCPU->al();
+	    break;
+	  case CL:
+	    std::cout << "\tREG "<< std::bitset<3>(REG) << " = CL" << std::endl;
+	    retval = fakeCPU->cl();
+	    break;
+	  case DL:
+	    std::cout << "\tREG "<< std::bitset<3>(REG) << " = DL" << std::endl;
+	    retval = fakeCPU->dl();
+	    break;
+	  case BL:
+	    std::cout << "\tREG "<< std::bitset<3>(REG) << " = BL" << std::endl;
+	    retval = fakeCPU->bl();
+	    break;
 
-      case AH:
-	std::cout << "\tREG "<< std::bitset<3>(REG) << " = AH" << std::endl;
-	retval = fakeCPU->ah();
+	  case AH:
+	    std::cout << "\tREG "<< std::bitset<3>(REG) << " = AH" << std::endl;
+	    retval = fakeCPU->ah();
+	    break;
+	  case CH:
+	    std::cout << "\tREG "<< std::bitset<3>(REG) << " = CH" << std::endl;
+	    retval = fakeCPU->ch();
+	    break;
+	  case DH:
+	    std::cout << "\tREG "<< std::bitset<3>(REG) << " = DH" << std::endl;
+	    retval = fakeCPU->dh();
+	    break;
+	  case BH:
+	    std::cout << "\tREG "<< std::bitset<3>(REG) << " = BH" << std::endl;
+	    retval = fakeCPU->bh();
 	break;
-      case CH:
-	std::cout << "\tREG "<< std::bitset<3>(REG) << " = CH" << std::endl;
-	retval = fakeCPU->ch();
-	break;
-      case DH:
-	std::cout << "\tREG "<< std::bitset<3>(REG) << " = DH" << std::endl;
-	retval = fakeCPU->dh();
-	break;
-      case BH:
-	std::cout << "\tREG "<< std::bitset<3>(REG) << " = BH" << std::endl;
-	retval = fakeCPU->bh();
-	break;
-
-      default:
+	  default:
 	std::cerr << "Unrecognized REG field of " << std::bitset<3>(REG) << std::endl;
       }
-
+      }
 
     return retval;
   }
